ef2fe057cc4cf8c117798eacb3869ef36d73d08e
theJonan <ivan@jonan.info>
2017-06-22 14:47:05 +0300

    - Initial debugging attempts for JurassiX

---
+++ JurassicExtension/VSIXTest/JurassicRunner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Jurassic;
+using System.IO;
+
+namespace JurassicExtensionTest
+{
+    public class JurassicRunner
+    {
+        private static void Main(string[] args)
+        {
+            ScriptEngine jurassic = new ScriptEngine();
+            jurassic.EnableDebugging = true;
+            jurassic.ExecuteFile("Script.js");
+        }
+    }
+}
+
